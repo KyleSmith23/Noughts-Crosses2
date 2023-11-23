d066eaba7c1c442375ba44dc9dd0bfa40df5b3bf
kyle- <kyle-smith1@outlook.com>
2023-11-23 21:34:34 +0000

    Board class started

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿public class Board
+{
+    private CellType[,] _cells = new CellType[3, 3];
+
+    public CellType GetCell(int row, int col)
+    { 
+        return _cells[row, col];
+    }
+
+    public bool SetCell(int row, int col, CellType celltype)
+    {
+        if (_cells[row, col] != CellType.Empty ) return false;
+
+        _cells[row, col] = celltype; return true;
+    }
+}
+
+public enum CellType
+{
+    Empty,
+    X,
+    O
+}
